fff81a09e2d7c95ee01a5d4ad704674ce8e38d80
vampirefu <w807098553@126.com>
2017-08-21 02:02:17 +0800

    commit test

--- HaircutDesigner/app/src/main/java/com/haircut/wwf/haircutdesigner/MainActivity.java
+++ HaircutDesigner/app/src/main/java/com/haircut/wwf/haircutdesigner/MainActivity.java
@@ -2,6 +2,7 @@ package com.haircut.wwf.haircutdesigner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -11,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
+
+=======
+>>>>>>> 88b13942cbda223a45b84bdd0e886a993fb54b3f
     }
+
+    void MaleClick(View v){}
+
+    void FemaleClick(View v){}
 }
---
+++ HaircutDesigner/app/src/main/java/com/haircut/wwf/haircutdesigner/haircutData/HaircytData.java
@@ -0,0 +1,18 @@
+package com.haircut.wwf.haircutdesigner.haircutData;
+
+/**
+ * Created by WWF on 2017/8/12.
+ */
+
+public class HaircytData {
+    private String photoPath;
+
+    public String getPhotoPath()
+    {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+}
